7ac23c77f1ea882d1d1ad1a6ecbccdc0e18075dd
XavierSamuelHuppe <XavierSamuelHuppe@users.noreply.github.com>
2020-08-13 16:33:06 -0400

    ajout fonction mystere

--- myCode.js
+++ myCode.js
@@ -6,9 +6,14 @@ function multiplication(a, b) {
   return a * b;
 }
 
+function mystere(a, b) {
+  return b + b + b;
+}
+
 function print(operation, a, b) {
   console.log(operation(a, b));
 }
 
 print(addition, 4, 4);
 print(multiplication, 4, 4);
+print(mystere, 2, 4);
